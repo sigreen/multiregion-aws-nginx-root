b72fa9b076ea0ff1b600a2779a2b93d5027cd40f
Simon Green <simongreen@Simons-MBP.localdomain>
2021-03-29 13:17:14 -0400

    Updated to module 1.3.0

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 module "nginx-module" {
   source  = "app.terraform.io/SIMONGREEN-training/nginx-module/aws"
-  version = "1.2.0"
+  version = "1.3.0"
   availability_zone = var.availability_zone
   instance_ami = var.instance_ami
   public_key_name = var.public_key_name
