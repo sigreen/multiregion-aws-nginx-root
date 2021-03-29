35ff363d951fe351a0ae3ef27df9263c10e06b86
Alex Schenck <linuxbeak@gmail.com>
2021-03-29 14:27:52 -0400

    Fixed main.tf to work with var.region
    Removed provider code block and relocated variable declaration inside module code block

--- main.tf
+++ main.tf
@@ -8,14 +8,11 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.region
-}
-
 module "nginx-module" {
   source  = "app.terraform.io/SIMONGREEN-training/nginx-module/aws"
   version = "1.3.0"
   availability_zone = var.availability_zone
   instance_ami = var.instance_ami
   public_key_name = var.public_key_name
+  region = var.region
 }
