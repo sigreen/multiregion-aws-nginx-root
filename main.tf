2e14de000415b1f0631304c42f73cd3d1955479a
Simon Green <simongreen@Simons-MBP.localdomain>
2021-03-31 10:36:38 -0400

    Added remote backend

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "SIMONGREEN-training"
 
     workspaces {
-      name = "nginx-test-deploy"
+      name = "nginx-dev-deploy"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
 variable "region" {
-  default = "us-east-2"
+  description = "AWS region"
 }
 
 variable "availability_zone" {
   description = "availability zone to create subnet"
-  default = "us-east-2a"
 }
 
 variable "instance_ami" {
   description = "AMI for aws EC2 instance"
-  default = "ami-0cf31d971a3ca20d6"
 }
 
 variable "public_key_name" {
-  default = "publicKey"
+  default = "sgreenPublicKey"
 }
